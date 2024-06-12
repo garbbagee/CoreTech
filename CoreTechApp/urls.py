5c39b8b156942eef9f3e2b7603fce3f3fae09369
garbbagee <romandiazmiguelignacio@gmail.com>
2024-06-12 11:12:42 -0400

    migracion de plantillas, index completo
    falta linkear todas las vistas y hacer el load static en todas las otras

--- CoreTechApp/urls.py
+++ CoreTechApp/urls.py
@@ -1,5 +1,23 @@
 from django.urls import path
-from .views import index
+from .views import index, acercade, carrocompras, cuenta, gama_alta, gama_media, gama_baja, torrega1, torrega2, torrega3, torrega4, torregb1, torregb2, torregb3, torregb4, torregm1, torregm2, torregm3, torregm4
 urlpatterns = [
   	path('',index,name="index"),
+    path('acercade/', acercade, name='acercade'),
+    path('carrocompras/', carrocompras, name='carrocompras'),
+    path('cuenta/', cuenta, name='cuenta'),
+    path('GamaAlta/', gama_alta, name='GamaAlta'),
+    path('GamaMedia/', gama_media, name='GamaMedia'),
+    path('GamaBaja/', gama_baja, name='GamaBaja'),
+    path('torrega1/', torrega1, name='torrega1'),
+    path('torrega2/', torrega2, name='torrega2'),
+    path('torrega3/', torrega3, name='torrega3'),
+    path('torrega4/', torrega4, name='torrega4'),
+    path('torregb1/', torregb1, name='torregb1'),
+    path('torregb2/', torregb2, name='torregb2'),
+    path('torregb3/', torregb3, name='torregb3'),
+    path('torregb4/', torregb4, name='torregb4'),
+    path('torregm1/', torregm1, name='torregm1'),
+    path('torregm2/', torregm2, name='torregm2'),
+    path('torregm3/', torregm3, name='torregm3'),
+    path('torregm4/', torregm4, name='torregm4'),
 ]
--- CoreTechApp/views.py
+++ CoreTechApp/views.py
@@ -1,5 +1,59 @@
 from django.shortcuts import render
 
 def index(request):
-    context = {}
-    return render(request,'html/index.html',context)
+    return render(request, 'html/index.html')
+
+def acercade(request):
+    return render(request, 'html/acercade.html')
+
+def carrocompras(request):
+    return render(request, 'html/carrocompras.html')
+
+def cuenta(request):
+    return render(request, 'html/cuenta.html')
+
+def gama_alta(request):
+    return render(request, 'html/GamaAlta.html')
+
+def gama_media(request):
+    return render(request, 'html/GamaMedia.html')
+
+def gama_baja(request):
+    return render(request, 'html/GamaBaja.html')
+
+# Agregar las vistas para los otros archivos HTML
+def torrega1(request):
+    return render(request, 'html/torrega1.html')
+
+def torrega2(request):
+    return render(request, 'html/torrega2.html')
+
+def torrega3(request):
+    return render(request, 'html/torrega3.html')
+
+def torrega4(request):
+    return render(request, 'html/torrega4.html')
+
+def torregb1(request):
+    return render(request, 'html/torregb1.html')
+
+def torregb2(request):
+    return render(request, 'html/torregb2.html')
+
+def torregb3(request):
+    return render(request, 'html/torregb3.html')
+
+def torregb4(request):
+    return render(request, 'html/torregb4.html')
+
+def torregm1(request):
+    return render(request, 'html/torregm1.html')
+
+def torregm2(request):
+    return render(request, 'html/torregm2.html')
+
+def torregm3(request):
+    return render(request, 'html/torregm3.html')
+
+def torregm4(request):
+    return render(request, 'html/torregm4.html')
